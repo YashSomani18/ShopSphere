3e37c2a4f0c29e65c8361ef659b5d7b6197ae98f
Yash Somani <101785058+YashSomani18@users.noreply.github.com>
2024-01-02 14:18:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,11 +13,22 @@ import CartPage from "./components/CartPage";
 import Header from "./components/Header";
 
 function App() {
-  const isLoggedIn = localStorage.getItem("token");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [cartItems, setCartItems] = useState([]);
 
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem("token"));
+  }, []);
   const addToCart = (product) => {
     setCartItems([...cartItems, product]);
+  };
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutSuccess = () => {
+    setIsLoggedIn(false);
   };
 
   const [sortOrder, setSortOrder] = useState("");
@@ -25,11 +36,18 @@ function App() {
   return (
     <Router>
       {isLoggedIn && (
-        <Header cartItems={cartItems} setSortOrder={setSortOrder} />
+        <Header
+          cartItems={cartItems}
+          setSortOrder={setSortOrder}
+          onLogoutSuccess={handleLogoutSuccess}
+        />
       )}
 
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login onLoginSuccess={handleLoginSuccess} />}
+        />
         <Route
           path="/home"
           element={<MainPage addToCart={addToCart} sortOrder={sortOrder} />}
